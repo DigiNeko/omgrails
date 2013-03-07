f937147ce54c55af77577e76480dd8162eda0b12
DigiNeko <inkwind07@gmail.com>
2013-03-06 21:32:13 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omgrails::Application.config.secret_token = 'f82d9ec819d8e8b3f993c3f9571aac4e67bdc736cebb89d3c724a88ce93cc00a8ec909ab13c23f8141fe8e483663e5c981e558f3e4a70bc8ceffa6efaddda2e2'
